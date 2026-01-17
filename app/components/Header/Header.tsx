6b9b51cc1d56ba0606d9057c5885303ed8259038
amandamayfield <amandamayfield.designs@gmail.com>
2026-01-16 18:03:30 -0600

    feat: adding header styling

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,12 +1,13 @@
+import './header.scss';
 import { Link } from 'react-router';
 
 export default function Header() {
   return (
-    <div>
-      <Link to={"/"} aria-label="Click to visit the Home page">Home</Link>
-      <Link to={"/about"} aria-label="Click to visit the About page">About</Link>
-      <Link to={"/contact"} aria-label="Click to visit the Contact page">Contact</Link>
-      <Link to={"/projects"} aria-label="Click to visit the Projects page">Projects</Link>
-    </div>
+    <header className="header">
+      <Link to={"/"} className="header-link" aria-label="Click to visit the Home page">Home</Link>
+      <Link to={"/about"} className="header-link" aria-label="Click to visit the About page">About</Link>
+      <Link to={"/contact"} className="header-link" aria-label="Click to visit the Contact page">Contact</Link>
+      <Link to={"/projects"} className="header-link" aria-label="Click to visit the Projects page">Projects</Link>
+    </header>
   );
 }
