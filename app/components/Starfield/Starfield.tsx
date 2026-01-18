77389c71c048485e300b3dd9a733f9fae37d2009
amandamayfield <amandamayfield.designs@gmail.com>
2026-01-18 10:33:36 -0600

    feat: shooting star animation

--- app/components/Starfield/Starfield.tsx
+++ app/components/Starfield/Starfield.tsx
@@ -1,12 +1,15 @@
-import './starfield.scss';
+import "./starfield.scss";
 
 export default function Starfield() {
   return (
     <div className="starfield">
-      <div className='starfield__stars'></div>
-      <div className='starfield__stars2'></div>
-      <div className='starfield__stars3'></div>
-      <div className='starfield__title'>
+      <span className="starfield__shoot"></span>
+      {/* <span className="starfield__shoot"></span>
+      <span className="starfield__shoot"></span> */}
+      <div className="starfield__stars"></div>
+      <div className="starfield__stars2"></div>
+      <div className="starfield__stars3"></div>
+      <div className="starfield__title">
         <h1>Amanda Mayfield</h1>
       </div>
     </div>
