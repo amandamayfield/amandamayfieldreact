749be126d1d052659ef66b2b8a54e44f7dc69560
amandamayfield <amandamayfield.designs@gmail.com>
2026-01-21 22:19:01 -0600

    feat: add header mobile menu

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,44 +1,75 @@
 import "./header.scss";
 import { Link } from "react-router";
+import { useState } from "react";
+import NavButton from "../navButton/navButton";
+import Logo from "../Logo/Logo";
+
+type NavLink = {
+  name: string;
+  link: string;
+};
+
+const navLinks: readonly NavLink[] = [
+  { name: "Home", link: "/" },
+  { name: "About", link: "/about" },
+  { name: "Blog", link: "/blog" },
+  { name: "Projects", link: "/projects" },
+  { name: "Contact", link: "/contact" },
+] as const;
 
 export default function Header() {
   return (
     <header className="header">
-      <Link
-        to={"/about"}
-        className="header-link"
-        aria-label="Click to visit the About page"
-      >
-        About
-      </Link>
-      <Link
-        to={"/contact"}
-        className="header-link"
-        aria-label="Click to visit the Contact page"
-      >
-        Contact
-      </Link>
-      <Link
-        to={"/"}
-        className="header-link"
-        aria-label="Click to visit the Home page"
-      >
-        Home
-      </Link>
-      <Link
-        to={"/projects"}
-        className="header-link"
-        aria-label="Click to visit the Projects page"
-      >
-        Projects
-      </Link>
-      <Link
-        to={"/blog"}
-        className="header-link"
-        aria-label="Click to visit the Blog page"
-      >
-        Blog
-      </Link>
+      {/* <DesktopNav /> */}
+      <MobileNav />
     </header>
+  );
+}
+
+function MobileNav() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Logo />
+      <NavButton isOpen={isOpen} onClick={toggleMenu} />
+      <nav className={`header-nav ${isOpen ? "isOpen" : ""}`}>
+        <ul className="header-links">
+          {navLinks.map((link) => (
+            <li key={link.name}>
+              <Link
+                to={link.link}
+                className="header-link"
+                aria-label="Click to visit the {link.name} page"
+                onClick={toggleMenu}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+function DesktopNav() {
+  return (
+    <nav className="header-nav">
+      <ul className="header-links">
+        {navLinks.map((link) => (
+          <li key={link.name}>
+            <Link
+              to={link.link}
+              className="header-link"
+              aria-label="Click to visit the {link.name} page"
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
