edae1f8af3d8c58672563a160eb2893073ed8ae4
amandamayfield <amandamayfield.designs@gmail.com>
2026-02-02 18:02:56 -0600

    feat: added the about code section

--- app/components/AboutCode/AboutCode.tsx
+++ app/components/AboutCode/AboutCode.tsx
@@ -3,7 +3,32 @@ import "./aboutCode.scss";
 export default function AboutCode() {
   return (
     <section className="about-code__contain">
-      About programming section
+      <h3 className="about-code__title">The Pivot to Technology</h3>
+      <div className="about-code__content">
+        <div className="about-code__bento polka-dot"></div>
+        <div className="about-code__bento">
+          <p>
+            While working on creating a website for my own fashion
+            line, I really enjoyed the aspect of styling the page and
+            interactions. I decided to take some classes at a local
+            community college and really fell in love.
+          </p>
+          <p>
+            I graduated with an Associates in Applied Science in 2018
+            while already thriving in my first role as a software
+            engineer. I fully jumped on this train and never looked
+            back.
+          </p>
+          <p>
+            I've grown to be especially passionate about Accessibility
+            and Test Driven Development. In 2025, I gave my first
+            public Tech talk about the "class" keyword in Javascript
+            and loved it. My hope is to conquer speaking at a
+            conference next.
+          </p>
+        </div>
+        <div className="about-code__bento polka-dot"></div>
+      </div>
     </section>
   );
 }
