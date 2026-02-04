572821db22152e902d44ea662c93627d9cae5b14
amandamayfield <amandamayfield.designs@gmail.com>
2026-02-03 18:16:58 -0600

    feat: added craft code section and styling

---
+++ app/components/CraftCode/CraftCode.tsx
@@ -0,0 +1,32 @@
+import "./craftCode.scss";
+
+export default function CraftCode() {
+  return (
+    <section className="craft-code__contain">
+      <h3 className="craft-code__title">
+        The intersection of Craft & Code
+      </h3>
+      <div className="craft-code__content">
+        <div className="craft-code__bento"></div>
+        <div className="craft-code__bento">
+          <p>
+            Though it may seem like quite the pivot, there's actually
+            so much in common between Tech and Fashion Design. Both
+            involve taking a design and bringing it to life, whether
+            that's making a 3 dimensional garment or an interactive
+            website experience.
+          </p>
+          <p>
+            The high level of detail and care while creating is
+            present in both disciplines. In fashion, a fraction of an
+            inch is the difference between a perfect fit and a failed
+            garment. I've carried that same mentality into software,
+            where I've learned that precision, usability, and quality
+            are non-negotiable.
+          </p>
+        </div>
+        <div className="craft-code__bento"></div>
+      </div>
+    </section>
+  );
+}
