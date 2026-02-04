17b32d93bce237e038f3a3216635187c07503206
amandamayfield <amandamayfield.designs@gmail.com>
2026-02-04 16:48:06 -0600

    feat: about fashion section and styling

--- app/components/AboutFashion/AboutFashion.tsx
+++ app/components/AboutFashion/AboutFashion.tsx
@@ -3,7 +3,30 @@ import "./aboutFashion.scss";
 export default function AboutFashion() {
   return (
     <section className="about-fashion__contain">
-      About fashion section
+      <h3 className="about-fashion__title">My history in Fashion</h3>
+      <div className="about-fashion__content">
+        <div className="about-fashion__bento">
+          <p>
+            I originally got a BFA in Fashion Design from The School
+            of the Art Institute of Chicago in 2010. I spent my time
+            there learning how to design, pattern, and assemble
+            garments, as well as haute couture sewing techniques.
+          </p>
+          <p>
+            After graduating, I explored the fashion industry for 7
+            years. I worked as a pattern maker for an up and coming
+            fitness wear company and a pattern maker and tailor at a
+            Highland Park couture shop.
+          </p>
+          <p>
+            My proudest moment by far was getting to be a seamstress
+            at the renowned couture house of Carolina Hererra.
+          </p>
+        </div>
+        <div className="about-fashion__bento"></div>
+        <div className="about-fashion__bento"></div>
+        <div className="about-fashion__bento"></div>
+      </div>
     </section>
   );
 }
