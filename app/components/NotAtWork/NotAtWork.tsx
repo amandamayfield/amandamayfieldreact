de5343eee65b224b7f7ad8160e66e6e6b635971f
amandamayfield <amandamayfield.designs@gmail.com>
2026-02-04 18:09:32 -0600

    feat: added not at work section and styling

--- app/components/NotAtWork/NotAtWork.tsx
+++ app/components/NotAtWork/NotAtWork.tsx
@@ -3,7 +3,38 @@ import "./notAtWork.scss";
 export default function NotAtWork() {
   return (
     <section className="naw__contain">
-      Not at work about section
+      <h3 className="naw__title">Life beyond Coding</h3>
+      <div className="naw__content">
+        <div className="naw__bento"></div>
+        <div className="naw__bento">
+          <p>
+            In 2019, I got to marry the love of my life, fellow
+            Engineer and maker enthusiast Adam. We have a grumpy old
+            cat named Freyja and two loud, wiggly corgis named
+            Splendid and Reeses.
+          </p>
+          <p>
+            When I'm not working, I occasionally make code side
+            projects for fun, but mostly devote my tiem to crafting! I
+            still love to sew and create costumes, clothing, and
+            historical garments.
+          </p>
+          <p>
+            My other big obsession is Halloween and adding to out ever
+            expanding front yard graveyard display. We make just about
+            everything in it from scratch.
+          </p>
+          <p>
+            I also enjoy traveling the world and scrapbooking those
+            trips, taking care of our indoor and outdoor plants,
+            baking cookies, and meditation. I also enjoy volunteering
+            for our local food pantry and registering people to vote.
+          </p>
+        </div>
+        <div className="naw__bento"></div>
+        <div className="naw__bento"></div>
+        <div className="naw__bento"></div>
+      </div>
     </section>
   );
 }
