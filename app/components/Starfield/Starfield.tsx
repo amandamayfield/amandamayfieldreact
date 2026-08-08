db5f983c115138edffef7cdeeac3d8c9bee6f25a
amandamayfield <amandamayfield.designs@gmail.com>
2026-08-08 11:14:29 -0500

    feat: updating star field to be full width

--- app/components/Starfield/Starfield.tsx
+++ app/components/Starfield/Starfield.tsx
@@ -3,16 +3,14 @@ import StarfieldTitle from "../StarfieldTitle/StarfieldTitle";
 
 export default function Starfield() {
   return (
-    <div className="starfield-contain">
-      <div className="starfield">
-        <span className="starfield__shoot"></span>
-        {/* <span className="starfield__shoot"></span>
+    <div className="starfield">
+      <span className="starfield__shoot"></span>
+      {/* <span className="starfield__shoot"></span>
         <span className="starfield__shoot"></span> */}
-        <div className="starfield__stars"></div>
-        <div className="starfield__stars2"></div>
-        <div className="starfield__stars3"></div>
-        <StarfieldTitle />
-      </div>
+      <div className="starfield__stars"></div>
+      <div className="starfield__stars2"></div>
+      <div className="starfield__stars3"></div>
+      <StarfieldTitle />
     </div>
   );
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,12 @@
 import type { Route } from "./+types/home";
 import Starfield from "~/components/Starfield/Starfield";
-import Skills from "~/components/Skills/Skills";
 import IntroHome from "~/components/IntroHome/IntroHome";
-import BentoDivider from "~/components/BentoDivider/BentoDivider";
 import IntroTitle from "~/components/IntroTitle/IntroTitle";
 import SocialsHome from "~/components/SocialsHome/SocialsHome";
+import AboutFashion from "~/components/AboutFashion/AboutFashion";
+import AboutCode from "~/components/AboutCode/AboutCode";
+import NotAtWork from "~/components/NotAtWork/NotAtWork";
+import CraftCode from "~/components/CraftCode/CraftCode";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Amanda Mayfield - Software Engineer" }];
@@ -14,14 +16,13 @@ export default function Home() {
   return (
     <main>
       <Starfield />
-      <BentoDivider />
       <IntroTitle />
-      <BentoDivider />
+      <AboutFashion />
       <IntroHome />
-      <BentoDivider />
-      <Skills />
-      <BentoDivider />
       <SocialsHome />
+      <CraftCode />
+      <AboutCode />
+      <NotAtWork />
     </main>
   );
 }
