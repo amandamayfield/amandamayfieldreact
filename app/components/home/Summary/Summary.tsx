169d86d78efcfb265f855b0c4c138f351d4303d6
amandamayfield <amandamayfield.designs@gmail.com>
2026-09-13 15:21:05 -0500

    feat: adding the summary section

---
+++ app/components/home/Summary/Summary.tsx
@@ -0,0 +1,13 @@
+import "./Summary.scss";
+
+export default function Summary() {
+  return (
+    <section className="summary__contain">
+      <p>
+        <span>M</span>y name is Amanda Mayfield. I'm a senior software
+        engineer with a love for teaching, speaking, mentorship, and
+        accessibility.
+      </p>
+    </section>
+  );
+}
