0d1c35da8df83dbb0a965a14c6e6a9dbe37b592a
amandamayfield <amandamayfield.designs@gmail.com>
2026-09-13 15:20:38 -0500

    fix: small layout fixes on the home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/home";
 import Starfield from "~/components/home/Starfield/Starfield";
 import IntroTitle from "~/components/home/IntroTitle/IntroTitle";
+import Summary from "~/components/home/Summary/Summary";
+import "../style/app.scss";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Amanda Mayfield - Software Engineer" }];
@@ -10,112 +12,10 @@ export default function Home() {
   return (
     <main>
       <Starfield />
-      <IntroTitle />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit. Nam
-        non maxime vero sunt dolore aliquip in esse tempore. Eu id qui
-        minim mollitia quas in similique autem est corrupti. Eiusmod
-        cum duis quibusdam ullamco harum nihil qui id. Tempore laboris
-        magna exercitation pariatur consequatur tempore quo iusto
-        illum exercitation. Nisi aliquip eu id est est sit mollitia.
-        Cupiditate et in est pariatur quo fugiat ullamco mollit.
-        Deserunt amet consequatur commodo quod et at cum est
-        cupidatat. Nostrud eiusmod proident incididunt ut atque. Quo
-        atque eos qui ipsum elit cupidatat dolor nostrud dolorem
-        deserunt dolor. Nisi excepteur dolor laboris tempor ullamco
-        cumque culpa lorem omnis. Eum mollit autem et cillum nihil
-        excepturi soluta.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit. Nam
-        non maxime vero sunt dolore aliquip in esse tempore. Eu id qui
-        minim mollitia quas in similique autem est corrupti. Eiusmod
-        cum duis quibusdam ullamco harum nihil qui id. Tempore laboris
-        magna exercitation pariatur consequatur tempore quo iusto
-        illum exercitation. Nisi aliquip eu id est est sit mollitia.
-        Cupiditate et in est pariatur quo fugiat ullamco mollit.
-        Deserunt amet consequatur commodo quod et at cum est
-        cupidatat. Nostrud eiusmod proident incididunt ut atque. Quo
-        atque eos qui ipsum elit cupidatat dolor nostrud dolorem
-        deserunt dolor. Nisi excepteur dolor laboris tempor ullamco
-        cumque culpa lorem omnis. Eum mollit autem et cillum nihil
-        excepturi soluta.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit. Nam
-        non maxime vero sunt dolore aliquip in esse tempore. Eu id qui
-        minim mollitia quas in similique autem est corrupti. Eiusmod
-        cum duis quibusdam ullamco harum nihil qui id. Tempore laboris
-        magna exercitation pariatur consequatur tempore quo iusto
-        illum exercitation. Nisi aliquip eu id est est sit mollitia.
-        Cupiditate et in est pariatur quo fugiat ullamco mollit.
-        Deserunt amet consequatur commodo quod et at cum est
-        cupidatat. Nostrud eiusmod proident incididunt ut atque. Quo
-        atque eos qui ipsum elit cupidatat dolor nostrud dolorem
-        deserunt dolor. Nisi excepteur dolor laboris tempor ullamco
-        cumque culpa lorem omnis. Eum mollit autem et cillum nihil
-        excepturi soluta.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit. Nam
-        non maxime vero sunt dolore aliquip in esse tempore. Eu id qui
-        minim mollitia quas in similique autem est corrupti. Eiusmod
-        cum duis quibusdam ullamco harum nihil qui id. Tempore laboris
-        magna exercitation pariatur consequatur tempore quo iusto
-        illum exercitation. Nisi aliquip eu id est est sit mollitia.
-        Cupiditate et in est pariatur quo fugiat ullamco mollit.
-        Deserunt amet consequatur commodo quod et at cum est
-        cupidatat. Nostrud eiusmod proident incididunt ut atque. Quo
-        atque eos qui ipsum elit cupidatat dolor nostrud dolorem
-        deserunt dolor. Nisi excepteur dolor laboris tempor ullamco
-        cumque culpa lorem omnis. Eum mollit autem et cillum nihil
-        excepturi soluta.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit. Nam
-        non maxime vero sunt dolore aliquip in esse tempore. Eu id qui
-        minim mollitia quas in similique autem est corrupti. Eiusmod
-        cum duis quibusdam ullamco harum nihil qui id. Tempore laboris
-        magna exercitation pariatur consequatur tempore quo iusto
-        illum exercitation. Nisi aliquip eu id est est sit mollitia.
-        Cupiditate et in est pariatur quo fugiat ullamco mollit.
-        Deserunt amet consequatur commodo quod et at cum est
-        cupidatat. Nostrud eiusmod proident incididunt ut atque. Quo
-        atque eos qui ipsum elit cupidatat dolor nostrud dolorem
-        deserunt dolor. Nisi excepteur dolor laboris tempor ullamco
-        cumque culpa lorem omnis. Eum mollit autem et cillum nihil
-        excepturi soluta.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit. Nam
-        non maxime vero sunt dolore aliquip in esse tempore. Eu id qui
-        minim mollitia quas in similique autem est corrupti. Eiusmod
-        cum duis quibusdam ullamco harum nihil qui id. Tempore laboris
-        magna exercitation pariatur consequatur tempore quo iusto
-        illum exercitation. Nisi aliquip eu id est est sit mollitia.
-        Cupiditate et in est pariatur quo fugiat ullamco mollit.
-        Deserunt amet consequatur commodo quod et at cum est
-        cupidatat. Nostrud eiusmod proident incididunt ut atque. Quo
-        atque eos qui ipsum elit cupidatat dolor nostrud dolorem
-        deserunt dolor. Nisi excepteur dolor laboris tempor ullamco
-        cumque culpa lorem omnis. Eum mollit autem et cillum nihil
-        excepturi soluta.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit. Nam
-        non maxime vero sunt dolore aliquip in esse tempore. Eu id qui
-        minim mollitia quas in similique autem est corrupti. Eiusmod
-        cum duis quibusdam ullamco harum nihil qui id. Tempore laboris
-        magna exercitation pariatur consequatur tempore quo iusto
-        illum exercitation. Nisi aliquip eu id est est sit mollitia.
-        Cupiditate et in est pariatur quo fugiat ullamco mollit.
-        Deserunt amet consequatur commodo quod et at cum est
-        cupidatat. Nostrud eiusmod proident incididunt ut atque. Quo
-        atque eos qui ipsum elit cupidatat dolor nostrud dolorem
-        deserunt dolor. Nisi excepteur dolor laboris tempor ullamco
-        cumque culpa lorem omnis. Eum mollit autem et cillum nihil
-        excepturi soluta.
-      </p>
+      <div className="page-layout">
+        <IntroTitle />
+        <Summary />
+      </div>
     </main>
   );
 }
